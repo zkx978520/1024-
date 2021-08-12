9366a8b57a1b4a296b2b648d56780be9ea238964
翟凯旋 <1013537650@qq.com>
2021-08-12 10:36:59 +0800

    分类

---
+++ src/pages/classify/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">分类页面</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+
+  methods: {},
+  created() {},
+  mounted() {},
+};
+</script>
+<style scoped></style>
