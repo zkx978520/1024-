750f7a0c1a94cb4ffbd5392cc7be7fdcaf41de80
xupanpan <2015758869@qq.com>
2021-08-13 18:49:41 +0800

    订单

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,304 +1,411 @@
 <template>
-
-    <div class='cart'>
-        <!-- 顶部 -->
-        <div class="header">
-            <p class="iconfont icon-houtui" @click="goback"></p>
-            <p class="p2">购物车({{list.length}})</p>
-            <p class="p3" @click="delshow">管理&nbsp;<span class="iconfont icon-msg"></span></p>
-        </div>
-        <!-- 商品列表-->
-        <ul>
-          <li v-for="(item,i) in list" :key="item._id+i">
-            <van-checkbox v-model="item.checked" checked-color="#ee0a24">商品{{i+1}}</van-checkbox>
-            <div class="lititle">
-              <img :src="item.product.coverImg" alt="">
-              <div class="maincon">
-                <h5>{{item.product.name}}</h5>
-                <div class="priceAnum">
-                  <h4><span>￥</span>{{item.product.price}}</h4>
-                  <p>
-                    <span @click="item.quantity > 1 && updatePro(item.product._id, -1)">-</span>
-                    <span class="number">{{item.quantity}}</span> 
-                    <span @click="updatePro(item.product._id, 1)">+</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-        <!-- 空状态 -->
-        <div class="empty">
-            <van-empty description="你的购物车空空的" v-show="play">
-              <van-button round type="danger" class="bottom-button" @click="goshop">去逛逛</van-button>
-            </van-empty>
-        </div>
-        <!-- 结算 -->
-        <div class="count">
-          <van-submit-bar :price="totalPrice * 100" button-text="提交订单" @submit="onSubmit" >
-            <van-checkbox v-model="checked" checked-color="#ee0a24">全选</van-checkbox>
-          </van-submit-bar>
-        </div>
-        <!-- 删除 -->
-        <div class="del" v-show="flag">
-          <van-checkbox  class="allcheck" v-model="checked" checked-color="#ee0a24">全选</van-checkbox>
-          <van-button class="delbtn" type="danger" round @click="delcarts">确&nbsp;定</van-button>
-        </div>
+  <div class="cart">
+    <!-- 顶部 -->
+    <div class="header">
+      <p class="iconfont icon-houtui" @click="goback"></p>
+      <p class="p2">购物车({{ list.length }})</p>
+      <p class="p3" @click="delshow">
+        管理&nbsp;<span class="iconfont icon-msg"></span>
+      </p>
     </div>
+    <!-- 商品列表-->
+    <ul>
+      <li v-for="(item, i) in list" :key="item._id + i">
+        <van-checkbox v-model="item.checked" checked-color="#ee0a24"
+          >商品{{ i + 1 }}</van-checkbox
+        >
+        <div class="lititle">
+          <img :src="item.product.coverImg" alt="" />
+          <div class="maincon">
+            <h5>{{ item.product.name }}</h5>
+            <div class="priceAnum">
+              <h4><span>￥</span>{{ item.product.price }}</h4>
+              <p>
+                <span
+                  @click="item.quantity > 1 && updatePro(item.product._id, -1)"
+                  >-</span
+                >
+                <span class="number">{{ item.quantity }}</span>
+                <span @click="updatePro(item.product._id, 1)">+</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <!-- 空状态 -->
+    <div class="empty">
+      <van-empty description="你的购物车空空的" v-show="play">
+        <van-button round type="danger" class="bottom-button" @click="goshop"
+          >去逛逛</van-button
+        >
+      </van-empty>
+    </div>
+    <!-- 结算 -->
+    <div class="count">
+      <van-submit-bar
+        :price="totalPrice * 100"
+        button-text="提交订单"
+        @submit="onSubmit"
+      >
+        <van-checkbox v-model="checked" checked-color="#ee0a24"
+          >全选</van-checkbox
+        >
+        <!-- 弹出层 -->
+        <van-popup
+          v-model="show"
+          round
+          position="bottom"
+          :style="{ height: '50%' }"
+          class="tanchu"
+        >
+          <van-address-list
+            v-model="chosenAddressId"
+            :list="addressList"
+            :add-button-text="addbuttontext"
+            @add="onAdd"
+            @edit="onEdit"
+            @click-item="onClickItem"
+          />
+        </van-popup>
+      </van-submit-bar>
+    </div>
+    <!-- 删除 -->
+    <div class="del" v-show="flag">
+      <van-checkbox class="allcheck" v-model="checked" checked-color="#ee0a24"
+        >全选</van-checkbox
+      >
+      <van-button class="delbtn" type="danger" round @click="delcarts"
+        >确&nbsp;定</van-button
+      >
+    </div>
+  </div>
 </template>
 
 <script>
-import {post} from "../../utils/request"
-import { addorder } from "../../api/order";
-import { loadCartlist, addToCart,  } from "../../api/cart";
+import { post } from "../../utils/request";
+import { addorder, adresslist, adressload } from "../../api/order";
+import { loadCartlist, addToCart } from "../../api/cart";
+import { Toast } from "vant";
 
 export default {
-    
-    components: {},
-    data() {
-        
-        return {
-          list:[],
-          play:false,
-          flag:false,
-        };
-    },
-    computed: {
-      // 全选
-      checked: {
-        set(flag) {
-          this.list.map((item) => this.$set(item, "checked", flag));
-         },
-        get() {
-          return (
-            this.list.length ==
-            this.list.filter((item) => item.checked == true).length
-          );
-        },
+  components: {},
+  data() {
+    return {
+      list: [],
+      play: false,
+      flag: false,
+      show: false,
+      chosenAddressId: "1",
+      addressList: [],
+      getAddressByIdList: [],
+      addbuttontext: "",
+    };
+  },
+  computed: {
+    // 全选
+    checked: {
+      set(flag) {
+        this.list.map((item) => this.$set(item, "checked", flag));
       },
-      // 总价
-      totalPrice() {
+      get() {
+        return (
+          this.list.length ==
+          this.list.filter((item) => item.checked == true).length
+        );
+      },
+    },
+    // 总价
+    totalPrice() {
       return this.list
         .filter((item) => {
           return item.checked;
         })
-        .reduce(function(lasttime, now) {
+        .reduce(function (lasttime, now) {
           return lasttime + now.product.price * now.quantity; //商品价格*购物车此商品的数量
         }, 0);
-      },
-        // 获取选中的购物车列表
-      selectgoods() {
-        var selectlist = [];
-        this.list.forEach((item) => {
-          if (item.checked) {
-            selectlist.push({
-              quantity: item.quantity,
-              product: item._id,
-              price: item.product.price,
-            });
-          }
-        });
-        return selectlist;
-      },
-
-      // 获取选中购物车的id
-        selectId() {
-        var idlist = [];
-        this.list.forEach((item) => {
-          if (item.checked) {
-            idlist.push(
-           item._id,
-            );
-          }
-        });
-        return idlist;
-      },
     },
-    watch: {},
-    
-    methods: {
-       //  加载购物车列表
-        async getcartlist() {
-              let token=localStorage.getItem("token")
-            console.log(token);
-        if (token==null) {
-            this.$router.push("/login")
-        }else{
-          const result = await loadCartlist();
-          console.log(result);
-          this.list = result.data;
-          if (result.data.length==0) {
-            this.play=true
-          }
+    // 获取选中的购物车列表
+    selectgoods() {
+      var selectlist = [];
+      this.list.forEach((item) => {
+        if (item.checked) {
+          selectlist.push({
+            quantity: item.quantity,
+            product: item._id,
+            price: item.product.price,
+          });
         }
-          
-        },
-      //  加载购物车列表
-        // async getcartlist() {
-        //   const result = await loadCartlist();
-        //   console.log(result);
-        //   this.list = result.data;
-        //   if (result.data.length==0) {
-        //     this.play=true
-        //   }
-        // },
+      });
+      return selectlist;
+    },
 
-        // 更新购物车数量
-      async updatePro(id, num) {
-        await addToCart(id, num);
-        this.list.forEach((data) => {
-          if (data.product._id == id) {
-            data.quantity = data.quantity + num;
-          }
-        });
-      },
-      // 提交订单
-      async onSubmit() {
-        // console.log(this.selectgoods);
-      const result = await addorder({
-        receiver: "张三",
-        regions: "123456778",
-        address: "55667788",
-        orderDetails: this.selectgoods,
+    // 获取选中购物车的id
+    selectId() {
+      var idlist = [];
+      this.list.forEach((item) => {
+        if (item.checked) {
+          idlist.push(item._id);
+        }
+      });
+      return idlist;
+    },
+  },
+  watch: {},
+
+  methods: {
+    //  加载购物车列表
+    async getcartlist() {
+      let token = localStorage.getItem("token");
+      console.log(token);
+      if (token == null) {
+        this.$router.push("/login");
+      } else {
+        const result = await loadCartlist();
+        console.log(result);
+        this.list = result.data;
+        if (result.data.length == 0) {
+          this.play = true;
+        }
+      }
+    },
+    //  加载购物车列表
+    // async getcartlist() {
+    //   const result = await loadCartlist();
+    //   console.log(result);
+    //   this.list = result.data;
+    //   if (result.data.length==0) {
+    //     this.play=true
+    //   }
+    // },
+
+    // 更新购物车数量
+    async updatePro(id, num) {
+      await addToCart(id, num);
+      this.list.forEach((data) => {
+        if (data.product._id == id) {
+          data.quantity = data.quantity + num;
+        }
+      });
+    },
+
+    //  批量删除
+    delshow() {
+      this.flag = true;
+    },
+    async delcarts() {
+      const result = await post("/api/v1/shop_carts/delmany", {
+        ids: this.selectId,
       });
       console.log(result);
-     },
-    //  批量删除
-       delshow(){
-      this.flag=true
+      await this.getcartlist();
+      this.flag = false;
     },
-      async delcarts(){
-          const result=await post("/api/v1/shop_carts/delmany",
-           {ids:this.selectId})
-           console.log(result);
-           await this.getcartlist();
-           this.flag=false;
-      },
-      // 返回上一页
-      goback(){
-        this.$router.go(-1)
-      },
-      // 空状态下跳转home
-      goshop(){
-        this.$router.push("/home")
-      },
+    // 返回上一页
+    goback() {
+      this.$router.go(-1);
     },
-    created() {
-        this.getcartlist();
+    // 空状态下跳转home
+    goshop() {
+      this.$router.push("/home");
     },
-    mounted() {
-        
+
+    // 提交订单
+    async onSubmit() {
+      this.show = true;
+      // console.log(this.selectgoods);
+      // const result = await addorder({
+      //   receiver: "张三",
+      //   regions: "123456778",
+      //   address: "55667788",
+      //   orderDetails: this.selectgoods,
+      // });
+      // console.log(result);
     },
+    //获取地址列表添加到addressList
+    async getaddress() {
+      const addres = await adresslist();
+      if (addres.data.addresses.length == 0) {
+        this.addbuttontext = "新增地址";
+      } else {
+        this.addbuttontext = "确认";
+        this.chosenAddressId = addres.data.addresses[0]._id;
+        addres.data.addresses.forEach((item) => {
+          this.addressList.push({
+            id: item._id,
+            name: item.receiver,
+            tel: item.mobile,
+            address: item.regions,
+          });
+        });
+      }
+    },
+
+    //提交订单
+    async onAdd() {
+      console.log(111);
+      console.log(this.getAddressByIdList);
+      this.show = false;
+      console.log(this.getAddressByIdList.receiver);
+      console.log(this.getAddressByIdList.regions);
+      console.log(this.getAddressByIdList.regions);
+      console.log(this.selectgoods);
+      if (this.addbuttontext == "确认") {
+        const res = await addorder({
+          receiver: this.getAddressByIdList.receiver,
+          regions: this.getAddressByIdList.regions,
+          address: this.getAddressByIdList.regions,
+          orderDetails: this.selectgoods,
+        });
+        console.log(res);
+        this.$router.push("/user/dingdan");
+        // if (res.data.code == "success") {
+        //   Toast.success("加入订单成功");
+        //   this.getcartlist();
+        //   setTimeout(() => {
+        //     this.$router.push("/user/dingdan");
+        //     // this.$router.push({
+        //     //   path: "/user/dingdan",
+        //     //   query: {
+        //     //     address: res.data.info.order,
+        //     //   },
+        //     // });
+        //   }, 2000);
+        // }
+      } else {
+        this.$router.push("/adress");
+      }
+    },
+
+    //点击地址获得id
+    async onClickItem(item) {
+      console.log(item);
+      const getaddres = await adressload(item.id);
+      console.log(getaddres.data);
+      this.getAddressByIdList = getaddres.data;
+    },
+    //修改地址
+    onEdit(item, index) {
+      Toast("编辑地址:" + index);
+    },
+  },
+  created() {
+    this.getcartlist();
+    if (this.addressList.length == 0) {
+      this.getaddress();
     }
+  },
+  mounted() {},
+};
 </script>
 <style scoped>
-html{
+html {
   background-color: #efefef;
 }
-.cart{
+.cart {
   background-color: #efefef;
   min-height: 700px;
 }
-    .header{
-      width: 100%;
-    height: 44px;
-    color: #5e5e5e;
-    /* padding: 0px 30px; */
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid #999;
-      background-color: #fafafa;
-      position: fixed;
-      left: 0;
-      top: 0;
-    }
-    /* .header .p1{
+.header {
+  width: 100%;
+  height: 44px;
+  color: #5e5e5e;
+  /* padding: 0px 30px; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #999;
+  background-color: #fafafa;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+/* .header .p1{
       padding-left: 35px;
     } */
-    .header .p2{
-        font-size: 18px;
-    }
-    .header .p3{
-        font-size: 14px;
-        padding-right: 15px;
-    }
-    /* 购物车列表 */
-    .cart{
-      padding-top: 45px;
-    }
-    .cart ul{
-      margin: 10px 10px 10px 10px;
-    }
-    .cart ul li{
-      margin-top: 10px;
-      background-color: #fff;
-      border-radius: 5px;
-      /* margin-bottom: 10px; */
-      /* padding-bottom: 30px; */
-      padding: 15px 20px;
-    }
-    .cart ul li img{
-      width: 90px;
-    }
-    .cart ul li .lititle{
-      margin-top: 20px;
-      display: flex;
-      justify-content: space-between;
-      /* align-items: center; */
-    }
-     .cart ul li .lititle .maincon{
-      display: flex;
-      flex-direction:column;
-      justify-content: space-between;
-    }
-    .cart ul li .lititle .maincon .priceAnum{
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-    .cart ul li .lititle .maincon .priceAnum h4{
-      color:#ff4466;
-      font-size: 16px;
-    }
-     .cart ul li .lititle .maincon .priceAnum h4 span{
-      color:#ff4466;
-      font-size: 12px;
-    }
-     .cart ul li .lititle .maincon .priceAnum p span{
-      display:inline-block;
-      width: 25px;
-      height: 22px;
-      font-size: 12px;
-      background-color: #f7f7f7;
-      font-size: 18px;
-      line-height: 22px;
-      text-align: center;
-    }
-    /* 空状态 */
-   .bottom-button {
-    width: 160px;
-    height: 40px;
-  }
-   /* 删除模块 */
-  .del{
-    width: 355.33px;
-    height: 50px;
-    padding: 0 16px;
-    background-color: #fff;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    z-index: 1000;
-     display: flex;
-      justify-content: space-between;
-      align-items: center;
-  }
-  .del .allcheck{
-    font-size: 16px;
-  }
-   .del .delbtn{
-     width: 120px;
-    font-size: 20px;
-  }
+.header .p2 {
+  font-size: 18px;
+}
+.header .p3 {
+  font-size: 14px;
+  padding-right: 15px;
+}
+/* 购物车列表 */
+.cart {
+  padding-top: 45px;
+}
+.cart ul {
+  margin: 10px 10px 10px 10px;
+}
+.cart ul li {
+  margin-top: 10px;
+  background-color: #fff;
+  border-radius: 5px;
+  /* margin-bottom: 10px; */
+  /* padding-bottom: 30px; */
+  padding: 15px 20px;
+}
+.cart ul li img {
+  width: 90px;
+}
+.cart ul li .lititle {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  /* align-items: center; */
+}
+.cart ul li .lititle .maincon {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.cart ul li .lititle .maincon .priceAnum {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.cart ul li .lititle .maincon .priceAnum h4 {
+  color: #ff4466;
+  font-size: 16px;
+}
+.cart ul li .lititle .maincon .priceAnum h4 span {
+  color: #ff4466;
+  font-size: 12px;
+}
+.cart ul li .lititle .maincon .priceAnum p span {
+  display: inline-block;
+  width: 25px;
+  height: 22px;
+  font-size: 12px;
+  background-color: #f7f7f7;
+  font-size: 18px;
+  line-height: 22px;
+  text-align: center;
+}
+/* 空状态 */
+.bottom-button {
+  width: 160px;
+  height: 40px;
+}
+/* 删除模块 */
+.del {
+  width: 355.33px;
+  height: 50px;
+  padding: 0 16px;
+  background-color: #fff;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.del .allcheck {
+  font-size: 16px;
+}
+.del .delbtn {
+  width: 120px;
+  font-size: 20px;
+}
 </style>
--- src/pages/user/dingdan.vue
+++ src/pages/user/dingdan.vue
@@ -1,137 +1,268 @@
 <template>
-  <div class="dingdan">
-    <van-nav-bar
-      title="订单列表"
-      left-text="返回"
-      right-text="购物车"
-      left-arrow
-      @click-left="onClickLeft"
-      @click-right="onClickRight"
-    />
-    <!--  -->
-    <van-tabs v-model="active">
-      <van-tab title="待付款">
-        <!-- <van-image
-          width="257"
-          height="129"
-          class="pic1"
-          src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
-        />
-        <p class="p1">还没有相关订单哦!</p> -->
-        <van-swipe-cell v-for="item in list" :key="item._id">
-          <van-card
-            :num="item.quantity"
-            :price="item.product.price"
-            :title="item.product.name"
-            class="goods-card"
-            :thumb="item.product.coverImg"
-          />
-          <template #right>
-            <van-button
-              square
-              text="删除"
-              type="danger"
-              class="delete-button"
-              @click="del(item._id)"
+  <div class="order">
+    <!-- 顶部导航 -->
+    <van-nav-bar right-text="" @click-left="onClickLeft">
+      <template #left>
+        <b style="font-weight: 900">&lt;</b>
+      </template>
+      <template #title>
+        <p style="font-weight: 900">确认订单</p>
+      </template>
+    </van-nav-bar>
+    <!-- 中部 -->
+    <div class="order-mid">
+      <!-- 选择地址-mianban -->
+      <van-action-sheet
+        @select="selectAddr"
+        v-model="show"
+        :actions="actions"
+        cancel-text="取消"
+        description="选择地址"
+        close-on-click-action
+      />
+      <!-- 收货人地址 -->
+      <div class="address">
+        <div class="ren-addr" @click="show = true">
+          <!-- <h3><span>姓名</span> <i>手机号</i></h3> -->
+          <h3>{{ actObj.name }}</h3>
+          <div class="area">
+            <h4>{{ actObj.subname }}</h4>
+          </div>
+        </div>
+        <div class="address-r">
+          <van-icon name="arrow" size="1.1em" />
+        </div>
+      </div>
+      <!-- 要购买商品 -->
+      <div class="buy-shop">
+        <van-tabs v-model="active">
+          <van-tab title="待付款">
+            <van-swipe-cell v-for="item in list" :key="item._id">
+              <!-- <van-card
+                :num="item.quantity"
+                :price="item.product.price"
+                :title="item.product.name"
+                class="goods-card"
+                :thumb="item.product.coverImg"
+              /> -->
+              <!-- 商品卡片 -->
+              <van-card
+                v-for="(item, index) in orderSingleItemDetails"
+                :key="index"
+                :num="item.quantity"
+                :price="item.price"
+                :title="item.product.name"
+                :thumb="item.product.coverImg"
+              />
+              <template #right>
+                <van-button
+                  square
+                  text="删除"
+                  type="danger"
+                  class="delete-button"
+                  @click="del(item._id)"
+                />
+              </template>
+            </van-swipe-cell>
+            <!--  -->
+          </van-tab>
+          <van-tab title="待发货"
+            ><van-image
+              width="257"
+              height="129"
+              class="pic1"
+              src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
             />
-          </template>
-        </van-swipe-cell>
-        <!--  -->
-      </van-tab>
-      <van-tab title="待发货"
-        ><van-image
-          width="257"
-          height="129"
-          class="pic1"
-          src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
-        />
-        <p class="p1">还没有相关订单哦!</p></van-tab
-      >
-      <van-tab title="待收货"
-        ><van-image
-          width="257"
-          height="129"
-          class="pic1"
-          src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
-        />
-        <p class="p1">还没有相关订单哦!</p></van-tab
-      >
-      <van-tab title="待评价"
-        ><van-image
-          width="257"
-          height="129"
-          class="pic1"
-          src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
-        />
-        <p class="p1">还没有相关订单哦!</p></van-tab
-      >
-      <van-tab title="售后"
-        ><van-image
-          width="257"
-          height="129"
-          class="pic1"
-          src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
-        />
-        <p class="p1">还没有相关订单哦!</p></van-tab
-      >
-    </van-tabs>
+            <p class="p1">还没有相关订单哦!</p></van-tab
+          >
+          <van-tab title="待收货"
+            ><van-image
+              width="257"
+              height="129"
+              class="pic1"
+              src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
+            />
+            <p class="p1">还没有相关订单哦!</p></van-tab
+          >
+          <van-tab title="待评价"
+            ><van-image
+              width="257"
+              height="129"
+              class="pic1"
+              src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
+            />
+            <p class="p1">还没有相关订单哦!</p></van-tab
+          >
+          <van-tab title="售后"
+            ><van-image
+              width="257"
+              height="129"
+              class="pic1"
+              src="https://s10.mogucdn.com/new1/v1/bmisc/b22401c122fbedee3b1f5d708127e9f0/194500731154.png"
+            />
+            <p class="p1">还没有相关订单哦!</p></van-tab
+          >
+        </van-tabs>
+      </div>
+      <!-- 商品总价 -->
+      <div class="pro-info"></div>
+    </div>
+    <!-- 提交订单 -->
+    <van-submit-bar :price="orderPrice * 100" button-text="提交订单" />
   </div>
 </template>
+
 <script>
-import { loadCartlist } from "../../api/cart";
-import { dele } from "../../utils/request";
+import { adresslist, getOrderlist, getOrderItem } from "../../api/order";
+// import { addorder } from "../../api/order";
+// import { get } from '../../utils/request';
+// import { Toast } from 'vant';
 export default {
   components: {},
   data() {
     return {
       active: 0,
       list: [],
+      show: false, //地址面板状态
+      actions: [], // 地址信息-面板选项列表
+      actObj: {}, // 回调地址
+      orderid: "", // 生成订单id
+      orderIdArr: [], // 订单数组
+      orderSingleItem: {}, // 订单详情
+      orderSingleItemDetails: [], // 订单详情中的多少个商品
+      orderPrice: 0, // 此订单总价
+      productListOrder: [], // 从提交的订单中获取提交的商品数组
+      name: "",
     };
   },
   computed: {},
   watch: {},
 
   methods: {
+    //   地址列表
+    async getAddrList() {
+      const res = await adresslist();
+      let addrArr = res.data.addresses;
+      //   console.log("地址列表信息", res);
+      //   console.log("地址列表数组", addrArr);
+      let actArr = [];
+      addrArr.forEach((item) => {
+        actArr.push({
+          name: item.receiver + item.mobile,
+          subname: item.regions,
+        });
+      });
+      this.actions = actArr;
+      this.actObj = this.actions[0];
+      //   console.log("地址数据结构", this.actions);
+    },
+    //   订单列表
+    async orderList() {
+      console.log(111);
+      const res = await getOrderlist({ per: 10 });
+      console.log(res);
+      console.log("订单列表", res.data.orders);
+      let arr = [];
+      res.data.orders.forEach((v, index) => {
+        arr.push({ index: index, id: v._id });
+      });
+      this.orderIdArr = arr;
+      //   console.log("订单数组", this.orderIdArr);
+      this.orderid = this.orderIdArr[0].id;
+      console.log("当前订单id：", this.orderid);
+      this.orderItem(); //   订单详情
+    },
+    //   订单详情
+    async orderItem() {
+      const res = await getOrderItem(this.orderid);
+      this.orderSingleItem = res.data;
+      this.orderSingleItemDetails = res.data.details;
+      this.orderPrice = res.data.order.price;
+      console.log("订单详情", this.orderSingleItem);
+      console.log("订单详情中的多少个商品", this.orderSingleItemDetails);
+      console.log("此订单总价", this.orderPrice);
+    },
     onClickLeft() {
-      this.$router.push("/user");
+      //   Toast("返回");
+      this.$router.go(-1); // 返回上一页
     },
-    onClickRight() {},
-    // 获取用户购物车数据
-    async shuju() {
-      const result = await loadCartlist({});
-      this.list = result.data;
-    },
-    // 删除购物车数据
-    async del(id) {
-      const result = await dele("/api/v1/shop_carts/" + id);
-      console.log(result);
-      this.shuju();
+    // 面板回调
+    selectAddr(action, index) {
+      this.actObj = action;
+      console.log("回调地址", this.actObj, index);
     },
   },
   created() {
-    this.shuju();
+    // console.log(this.$route.query);
+    this.orderList(); //   订单列表
+    this.getAddrList(); //地址列表
   },
   mounted() {},
 };
 </script>
+
 <style scoped>
-.p1 {
-  font-size: 16px;
-  margin-top: 20px;
-
-  margin-left: 120px;
+.order {
+  background: rgb(244, 244, 244);
 }
-.pic1 {
-  margin-top: 50px;
-  margin-left: 60px;
+.order-mid {
+  /* background: greenyellow; */
+  /* min-height: 2rem; */
+  height: 12rem;
+  margin: 0.234rem 0.234rem 0;
 }
-
-.goods-card {
-  margin: 0;
-  background-color: #fff;
+.order-mid .address {
+  height: 1.3rem;
+  border-radius: 0.187rem;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.316rem 0.234rem;
 }
-
-.delete-button {
+/* 收货人地址 */
+.address .ren-addr {
+  flex: 1;
+  width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  /* background: coral; */
+  flex-direction: column;
+}
+.ren-addr h3 {
+  /* background: darkgray; */
+  font-size: 0.328rem;
+  font-weight: 900;
+}
+.ren-addr h3 span,
+i {
+  display: inline-block;
+}
+.ren-addr h3 i {
+  margin-left: 0.328rem;
+}
+.ren-addr .area {
+  font-size: 0.282rem;
+  color: #333;
+  margin-top: 0.164rem;
+}
+/* 购买商品 */
+.order-mid .buy-shop {
+  /* background: coral; */
+  margin: 0.234rem 0;
+}
+.buy-shop .van-card {
+  border-radius: 0.187rem;
+  background: white;
+}
+/* 商品标题 */
+.van-multi-ellipsis--l2 {
+  font-size: 0.328rem;
+}
+/* 商品信息 */
+.order-mid .pro-info {
+  background: yellow;
+  border-radius: 0.187rem;
 }
 </style>
